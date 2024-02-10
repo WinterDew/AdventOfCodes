00f8933f022401cc9ebb0c3e9c0b7714ff4d4fa9
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2024-02-10 12:33:44 +0530

    Update boilerplate and day 1

--- 2023/Boilerplate/Boilerplate.cpp
+++ 2023/Boilerplate/Boilerplate.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
 
+void println(const std::string message){
+    std::cout << message << std::endl;
+}
 void println(const char* message){
     std::cout << message << std::endl;
 }
 
+std::vector<std::string> readInput(const char* filename = "input.txt"){
+    std::vector<std::string> text;
+    std::string line;
+    std::ifstream file;
+    file.open(filename);
+    while(std::getline(file,line)){
+        text.push_back(line);
+    }
+    return text;
+}
+
+
 int main(){
+    
     println("Hello World!");
     return 0;
 }
