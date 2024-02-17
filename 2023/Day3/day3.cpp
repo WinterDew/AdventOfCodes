f7892e5230341db2a6400580451fb72639907118
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2024-02-17 15:57:47 +0530

    Abandoning day3 temporarily

--- 2023/Day3/day3.cpp
+++ 2023/Day3/day3.cpp
@@ -2,11 +2,30 @@
 #include<fstream>
 #include<vector>
 #include "../../utils/utils.h"
+using str = std::string;
+using vec = std::vector<str>;
 
-
+// IMPORTANT ccurrently abandoning day3 since I've got no idea on how to do it.
+// will come back to it later.
 
 int main(){
-    
-    println("Hello World!");
+    auto text = readInput("sample.txt");
+    for( int lno = 0; lno < text.size(); lno++){
+        str line = text[lno];
+        int current_no;
+        int counter = 0;
+        println(line);
+        for( int chrno = 0; chrno < line.size(); chrno++){
+            char chr = line[chrno];
+            if(std::isdigit(chr)){
+                while(std::isdigit(line[chrno + counter])){
+                    counter++;
+                }
+                current_no = std::stoi(line.substr(chrno,chrno+counter));
+                println(current_no);
+            }
+        }
+    }
+
     return 0;
 }
---
+++ 2023/Day3/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ day3.cpp -o day3.exe && ./day3.exe && rm day3.exe
